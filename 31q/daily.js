e8e2aeae5285ce92b1c8051553554c43b5706788
pe-hu <we.are.pe.hu@gmail.com>
2022-09-02 01:28:59 +0900

    Update daily.js

--- 31q/daily.js
+++ 31q/daily.js
@@ -1,5 +1,5 @@
 function daily(){
 mydate=new Date();
 num=mydate.getDate()%32;
-document.write('<img src="https://creative-community.space/pehu/31q/img/'+num+'.jpg">');
+document.write('<img src="https://vg.pe.hu/jp/31q/img/'+num+'.jpg">');
 }
