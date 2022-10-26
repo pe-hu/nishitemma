46cfab08d0b74e93750c6ce916cbe1752c2fc2c8
∧° ┐ <we.are.pe.hu@gmail.com>
2022-10-26 16:41:48 +0900

    31q

--- 31q/daily.js
+++ 31q/daily.js
@@ -1,7 +1,7 @@
+daily();
+
 function daily() {
     mydate = new Date();
-    num = mydate.getDate() % 32;
+    num = mydate.getDate();
     document.write('<img class="center" src="https://vg.pe.hu/jp/31q/img/' + num + '.jpg">');
 }
-
-daily();
