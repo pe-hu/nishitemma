e5702a748e13bb0ccd3fc4dcd2f881e72a9c934e
∧° ┐ <we.are.pe.hu@gmail.com>
2022-10-26 14:52:44 +0900

    31q

--- 31q/daily.js
+++ 31q/daily.js
@@ -1,5 +1,5 @@
-function daily(){
-mydate=new Date();
-num=mydate.getDate()%32;
-document.write('<img width="100%" src="https://vg.pe.hu/jp/31q/img/'+num+'.jpg">');
+function daily() {
+    mydate = new Date();
+    num = mydate.getDate() % 32;
+    document.write('<img src="https://vg.pe.hu/jp/31q/img/' + num + '.jpg">');
 }
--- 31q/scrollable.js
+++ 31q/scrollable.js
@@ -1,7 +1,7 @@
-jQuery.prototype.mousedragscrollable = function() {
+jQuery.prototype.mousedragscrollable = function () {
     let target; // 動かす対象
-    $(this).each(function(i, e) {
-        $(e).mousedown(function(event) {
+    $(this).each(function (i, e) {
+        $(e).mousedown(function (event) {
             event.preventDefault();
             target = $(e); // 動かす対象
             $(e).data({
@@ -15,14 +15,14 @@ jQuery.prototype.mousedragscrollable = function() {
             return false
         });
         // move後のlink無効
-        $(e).click(function(event) {
+        $(e).click(function (event) {
             if ($(e).data("move")) {
                 return false
             }
         });
     });
     // list要素内/外でのevent
-    $(document).mousemove(function(event) {
+    $(document).mousemove(function (event) {
         if ($(target).data("down")) {
             event.preventDefault();
             let move_x = $(target).data("x") - event.clientX;
@@ -34,7 +34,7 @@ jQuery.prototype.mousedragscrollable = function() {
             $(target).scrollTop($(target).data("scrolltop") + move_y);
             return false
         }
-    }).mouseup(function(event) {
+    }).mouseup(function (event) {
         $(target).data("down", false);
         return false;
     });
