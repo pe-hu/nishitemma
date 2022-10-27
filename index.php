6ddcb5d96e853c391cd098b9cf8ffb6d891582f3
∧° ┐ <we.are.pe.hu@gmail.com>
2022-10-27 19:36:05 +0900

    jp

--- index.php
+++ index.php
@@ -52,7 +52,6 @@ fclose($fp);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no">
   <title>∧° ┐ | creative, community space</title>
-  <link rel="stylesheet" href="style.css" />
   <link rel="stylesheet" href="calendar.css" />
   <link rel="stylesheet" href="collection.css" />
 </head>
