3b074b90630f16b18b28aa69f00db4380228a400
SK-Ⅱ <sasajimakazuma@gmail.com>
2022-10-28 18:39:59 +0900

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ fclose($fp);
 </head>
 
 <body>
-  <form id="collection" method="GET">
+  <form id="index" method="GET">
     <select id="calendar" name="month">
       <option disabled selected hidden>∧°┐</option>
       <option value="1">1月</option>
