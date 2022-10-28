132a401135517a3e31905c01d248ed8bfe613ddd
SK-Ⅱ <sasajimakazuma@gmail.com>
2022-10-28 18:53:24 +0900

    Update index.php

--- index.php
+++ index.php
@@ -124,7 +124,6 @@ fclose($fp);
     <?php else : ?>
     <?php endif; ?>
   </ul>
-  <hr />
   <div id="menu"></div>
   <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
   <script type="text/javascript">
