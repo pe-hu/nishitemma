aae5913c3f53461ac03d20029b555dda1744c1f4
SK-Ⅱ <sasajimakazuma@gmail.com>
2022-10-31 16:41:36 +0900

    jp

--- index.php
+++ index.php
@@ -49,80 +49,82 @@ fclose($fp);
 </head>
 
 <body id="box">
-  <div id="screensaver" class="screensaver">
-    <h1>We've moved to<br />
-      <b>creative-community.space</b>
-    </h1>
-  </div>
+  <main>
+    <div id="screensaver" class="screensaver">
+      <h1>We've moved to<br />
+        <b>creative-community.space</b>
+      </h1>
+    </div>
 
-  <form id="index_form" method="GET">
-    <select id="calendar" name="month">
-      <option disabled selected hidden>∧°┐</option>
-      <option value="1">1月</option>
-      <option value="2">2月</option>
-      <option value="3">3月</option>
-      <option value="4">4月</option>
-      <option value="5">5月</option>
-      <option value="6">6月</option>
-      <option value="7">7月</option>
-      <option value="8">8月</option>
-      <option value="9">9月</option>
-      <option disabled value="10">10月</option>
-      <option disabled value="11">11月</option>
-      <option disabled value="12">12月</option>
-    </select>
-    <h1 class="month">
-      <?php
-      date_default_timezone_set('Asia/Tokyo');
-      print(date($month) . "月")
-      ?>
-    </h1>
-    <button type="submit" name="submit">
-      <?php
-      print(date($year))
-      ?>
-    </button>
-  </form>
+    <form id="index_form" method="GET">
+      <select id="calendar" name="month">
+        <option disabled selected hidden>∧°┐</option>
+        <option value="1">1月</option>
+        <option value="2">2月</option>
+        <option value="3">3月</option>
+        <option value="4">4月</option>
+        <option value="5">5月</option>
+        <option value="6">6月</option>
+        <option value="7">7月</option>
+        <option value="8">8月</option>
+        <option value="9">9月</option>
+        <option disabled value="10">10月</option>
+        <option disabled value="11">11月</option>
+        <option disabled value="12">12月</option>
+      </select>
+      <h1 class="month">
+        <?php
+        date_default_timezone_set('Asia/Tokyo');
+        print(date($month) . "月")
+        ?>
+      </h1>
+      <button type="submit" name="submit">
+        <?php
+        print(date($year))
+        ?>
+      </button>
+    </form>
 
-  <ul class="date">
-    <?php if (!empty($rows)) : ?>
-      <?php foreach ($rows as $row) : ?>
-        <li class="<?= h($row[0]) ?>">
-          <b class="day">
-            <?= h($row[1]) ?>
-          </b>
-          <p class="<?= h($row[2]) ?>">
-            <i></i><br>
-            <u>
-              <?= h($row[3]) ?>
-            </u>
-          </p>
-          <p class="<?= h($row[4]) ?>">
-            <i></i><br>
-            <u>
-              <?= h($row[5]) ?>
-            </u>
-          </p>
-          <p class="<?= h($row[6]) ?>">
-            <i></i>
-            <br>
-            <u>
-              <?= h($row[7]) ?>
-            </u>
-            <span>
-              <?= h($row[8]) ?>
-            </span>
-          </p>
-          <p class="<?= h($row[9]) ?>">
-            <u><?= h($row[10]) ?></u>
-            <i><?= h($row[12]) ?></i>
-            <span><?= h($row[11]) ?></span>
-          </p>
-        </li>
-      <?php endforeach; ?>
-    <?php else : ?>
-    <?php endif; ?>
-  </ul>
+    <ul class="date">
+      <?php if (!empty($rows)) : ?>
+        <?php foreach ($rows as $row) : ?>
+          <li class="<?= h($row[0]) ?>">
+            <b class="day">
+              <?= h($row[1]) ?>
+            </b>
+            <p class="<?= h($row[2]) ?>">
+              <i></i><br>
+              <u>
+                <?= h($row[3]) ?>
+              </u>
+            </p>
+            <p class="<?= h($row[4]) ?>">
+              <i></i><br>
+              <u>
+                <?= h($row[5]) ?>
+              </u>
+            </p>
+            <p class="<?= h($row[6]) ?>">
+              <i></i>
+              <br>
+              <u>
+                <?= h($row[7]) ?>
+              </u>
+              <span>
+                <?= h($row[8]) ?>
+              </span>
+            </p>
+            <p class="<?= h($row[9]) ?>">
+              <u><?= h($row[10]) ?></u>
+              <i><?= h($row[12]) ?></i>
+              <span><?= h($row[11]) ?></span>
+            </p>
+          </li>
+        <?php endforeach; ?>
+      <?php else : ?>
+      <?php endif; ?>
+    </ul>
+  </main>
 
   <div id="floor"></div>
   <div id="index"></div>
