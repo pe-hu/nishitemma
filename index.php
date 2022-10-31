b894e933bd68e7a28c90b9dc4adae9e6515d47b2
SK-Ⅱ <sasajimakazuma@gmail.com>
2022-10-31 13:20:42 +0900

    Update index.php

--- index.php
+++ index.php
@@ -127,7 +127,7 @@ fclose($fp);
   <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
   <script type="text/javascript">
     $(function() {
-      $("#menu").load("menu.html");
+      $("#menu").load("nishitemma.html");
     })
 
     let btn = document.querySelector('#btn');
