4ece0820567dbb3db74709319b239b71885ae1b4
SK-Ⅱ <sasajimakazuma@gmail.com>
2022-10-31 17:44:48 +0900

    jp

--- dm/slideshow.js
+++ dm/slideshow.js
@@ -1,30 +1,30 @@
 const images = document.getElementById('loop_wrap');
 
 let img_src = [
-    "img/000.jpg",
-    "img/001.jpg",
-    "img/002.jpg",
-    "img/003.jpg",
-    "img/004.jpg",
-    "img/005.jpg",
-    "img/006.jpg",
-    "img/007.jpg",
-    "img/008.jpg",
-    "img/009.jpg",
-    "img/010.jpg",
-    "img/011.jpg",
-    "img/012.jpg",
-    "img/013.jpg",
-    "img/014.jpg",
-    "img/015.jpg",
-    "img/016.jpg",
-    "img/017.jpg",
-    "img/018.jpg",
-    "img/019.jpg",
-    "img/020.jpg",
-    "img/021.jpg",
-    "img/022.jpg",
-    "img/023.jpg"
+    "/jp/dm/img/000.jpg",
+    "/jp/dm/img/001.jpg",
+    "/jp/dm/img/002.jpg",
+    "/jp/dm/img/003.jpg",
+    "/jp/dm/img/004.jpg",
+    "/jp/dm/img/005.jpg",
+    "/jp/dm/img/006.jpg",
+    "/jp/dm/img/007.jpg",
+    "/jp/dm/img/008.jpg",
+    "/jp/dm/img/009.jpg",
+    "/jp/dm/img/010.jpg",
+    "/jp/dm/img/011.jpg",
+    "/jp/dm/img/012.jpg",
+    "/jp/dm/img/013.jpg",
+    "/jp/dm/img/014.jpg",
+    "/jp/dm/img/015.jpg",
+    "/jp/dm/img/016.jpg",
+    "/jp/dm/img/017.jpg",
+    "/jp/dm/img/018.jpg",
+    "/jp/dm/img/019.jpg",
+    "/jp/dm/img/020.jpg",
+    "/jp/dm/img/021.jpg",
+    "/jp/dm/img/022.jpg",
+    "/jp/dm/img/023.jpg"
 ];
 
 for (var i = 0; i < 2; i++) {
@@ -33,7 +33,4 @@ for (var i = 0; i < 2; i++) {
         img_element.src = img_src[count];
         images.appendChild(img_element);
     }
-} 
-
-const box = document.querySelectorAll(".box");
-box.style.cssText = 'bule';
+}
