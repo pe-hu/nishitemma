7fbcf5b1c7553097a3dd2112517c2a021a279731
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-09-08 13:04:20 +0900

    pehu

--- show/js/index.js
+++ show/js/index.js
@@ -1,46 +1,69 @@
 'use strict'
 
-const head = document.querySelector('head');
-const dataTime = document.querySelector('#date');
-
-dataTime.textContent = thisDate
-dataTime.setAttribute("data-time", thisDatetime)
-document.querySelector('#title').textContent = thisTitle
+const indexHead = document.querySelector('head');
 const indexTitle = document.createElement('title');
 const ogTitle = document.createElement('meta');
 indexTitle.textContent = thisTitle;
 ogTitle.setAttribute("property", "og:title");
 ogTitle.setAttribute("content", thisTitle);
-head.appendChild(indexTitle);
-head.appendChild(ogTitle);
-
-document.querySelector('#description').textContent = thisDescription
-document.querySelector('#door').textContent = thisDoor
+indexHead.appendChild(indexTitle);
+indexHead.appendChild(ogTitle);
 const indexDescription = document.createElement('meta');
 const ogDescription = document.createElement('meta');
 indexDescription.setAttribute("name", "description");
 indexDescription.setAttribute("content", thisDate + ' | ' + thisDescription + ' | ' + thisDoor);
 ogDescription.setAttribute("property", "og:description");
 ogDescription.setAttribute("content", thisDate + ' | ' + thisDescription + ' | ' + thisDoor);
-head.appendChild(indexDescription);
-head.appendChild(ogDescription);
+indexHead.appendChild(indexDescription);
+indexHead.appendChild(ogDescription);
 
-dataTime.addEventListener('click', function(event) {
-    let ago = new Date(thisDatetime);
-    let diff = new Date().getTime() - ago.getTime();
+const indexIcon = document.createElement("link");
+indexIcon.rel = "icon";
+indexIcon.href = favicon;
+indexHead.appendChild(indexIcon);
 
-    let progress = new Date(diff);
-    if (progress.getUTCFullYear() - 1970) {
-        event.target.textContent = progress.getUTCFullYear() - 1970 + '年前';
-    } else if (progress.getUTCMonth()) {
-        event.target.textContent = progress.getUTCMonth() + 'ヶ月前';
-    } else if (progress.getUTCDate() - 1) {
-        event.target.textContent = progress.getUTCDate() - 1 + '日前';
-    } else if (progress.getUTCHours()) {
-        event.target.textContent = progress.getUTCHours() + '時間前';
-    } else if (progress.getUTCMinutes()) {
-        event.target.textContent = progress.getUTCMinutes() + '分前';
-    } else {
-        event.target.textContent = 'たった今';
-    }
+document.addEventListener("DOMContentLoaded", function () {
+    const dataTime = document.querySelector('#date');
+    dataTime.textContent = thisDate;
+    dataTime.setAttribute("data-time", thisDatetime);
+    document.querySelector('#title').textContent = thisTitle;
+    document.querySelector('#description').textContent = thisDescription;
+    document.querySelector('#door').textContent = thisDoor;
+
+    dataTime.addEventListener('click', function (event) {
+        let ago = new Date(thisDatetime);
+        let diff = new Date().getTime() - ago.getTime();
+
+        let progress = new Date(diff);
+        if (progress.getUTCFullYear() - 1970) {
+            event.target.textContent = progress.getUTCFullYear() - 1970 + '年前';
+        } else if (progress.getUTCMonth()) {
+            event.target.textContent = progress.getUTCMonth() + 'ヶ月前';
+        } else if (progress.getUTCDate() - 1) {
+            event.target.textContent = progress.getUTCDate() - 1 + '日前';
+        } else if (progress.getUTCHours()) {
+            event.target.textContent = progress.getUTCHours() + '時間前';
+        } else if (progress.getUTCMinutes()) {
+            event.target.textContent = progress.getUTCMinutes() + '分前';
+        } else {
+            event.target.textContent = 'たった今';
+        }
+    });
+
+    const backBtn = document.createElement('button');
+    backBtn.id = "backBtn"
+    backBtn.className = "noprint"
+    backBtn.textContent = "↵"
+    backBtn.type = "button"
+    backBtn.style.position = "fixed"
+    backBtn.style.bottom = "0.5rem"
+    backBtn.style.right = "0.5rem"
+    backBtn.style.fontSize = "200%"
+    backBtn.style.width = "2.5rem"
+    backBtn.style.height = "2.5rem"
+    document.body.appendChild(backBtn);
+
+    backBtn.addEventListener('click', function () {
+        history.back(-1);
+    });
 });
---
+++ show/js/randomHEX.js
@@ -0,0 +1,17 @@
+function randomHEX() {
+    let code = '0123456789ABCDEF';
+    let str = '#';
+    for (let i = 0; i < 6; i++) {
+        str += code[Math.floor(Math.random() * code.length)];
+    }
+    return str;
+}
+
+const hexAll = document.querySelectorAll('.hexColor');
+hexAll.forEach(hexEach => {
+    hexEach.style.color = randomHEX()
+
+    hexEach.addEventListener('mouseover', (event) => {
+        hexEach.style.color = randomHEX()
+    }, false);
+});
