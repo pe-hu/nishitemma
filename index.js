05a310d227147f643138992135081cd6a3f4deaa
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-09-08 22:20:51 +0900

    pehu

--- index.js
+++ index.js
@@ -13,21 +13,13 @@ async function indexJSON(requestURL) {
 function indexHead(obj) {
     const head = document.querySelector('head');
     const indexTitle = document.createElement('title');
-    const ogTitle = document.createElement('meta');
     indexTitle.textContent = obj['title'];
-    ogTitle.setAttribute("property", "og:title");
-    ogTitle.setAttribute("content", obj['title']);
     head.appendChild(indexTitle);
-    head.appendChild(ogTitle);
     
     const indexDescription = document.createElement('meta');
-    const ogDescription = document.createElement('meta');
     indexDescription.setAttribute("name", "description");
     indexDescription.setAttribute("content", obj['description']);
-    ogDescription.setAttribute("property", "og:description");
-    ogDescription.setAttribute("content", obj['description']);
     head.appendChild(indexDescription);
-    head.appendChild(ogDescription);
 
     const indexAuthor = document.createElement("meta");
     indexAuthor.setAttribute("name", "author");
@@ -38,26 +30,6 @@ function indexHead(obj) {
     indexEmail.setAttribute("name", "reply-to");
     indexEmail.setAttribute("content", obj['email']);
     head.appendChild(indexEmail);
-
-    const ogType = document.createElement("meta");
-    ogType.setAttribute("property", "og:type");
-    ogType.setAttribute("content", obj['type']);
-    head.appendChild(ogType);
-
-    const ogIMG = document.createElement("meta");
-    ogIMG.setAttribute("property", "og:image");
-    ogIMG.setAttribute("content", obj['src']);
-    head.appendChild(ogIMG);
-
-    const ogSite = document.createElement("meta");
-    ogSite.setAttribute("property", "og:site_name");
-    ogSite.setAttribute("content", location.hostname);
-    head.appendChild(ogSite);
-
-    const ogURL = document.createElement("meta");
-    ogURL.setAttribute("property", "og:url");
-    ogURL.setAttribute("content", location.href);
-    head.appendChild(ogURL);
 
     const iconCC = document.createElement("link");
     iconCC.rel = "icon";
@@ -85,6 +57,23 @@ function indexORG(obj) {
         navORG.appendChild(inputORG);
         navORG.appendChild(labelORG);
     }
+}
+
+function indexItems(obj) {
+    const contents = document.querySelector('#contents');
+    const contentsAll = obj.contents;
+
+    for (const content of contentsAll) {
+        const contentsLi = document.createElement('li');
+        contentsLi.setAttribute("data-org", content.org);
+        contentsLi.innerHTML = `
+        <time>${content.date}</time>
+        <h3>${content.name}</h3>
+        <small>${content.description}</small>
+        <a style="display:${content.link};" href="${content.href}"></a>
+        `
+        contents.appendChild(contentsLi);
+    }
 
     window.onload = function() {
       let targets = document.querySelectorAll("#contents li")
@@ -111,23 +100,6 @@ function indexORG(obj) {
         })
       }
     };
-}
-
-function indexItems(obj) {
-    const contents = document.querySelector('#contents');
-    const contentsAll = obj.contents;
-
-    for (const content of contentsAll) {
-        const contentsLi = document.createElement('li');
-        contentsLi.setAttribute("data-org", content.org);
-        contentsLi.innerHTML = `
-        <time>${content.date}</time>
-        <h3>${content.name}</h3>
-        <small>${content.description}</small>
-        <a style="display:${content.link};" href="${content.href}"></a>
-        `
-        contents.appendChild(contentsLi);
-    }
 }
 
 async function fetchHTML(url = '', query = '') {
