f03f34bf80b2b9cb8a8f16a081ca7f48af2839d5
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-09-22 23:42:16 +0900

    2019

--- 2019/screensaver.js
+++ 2019/screensaver.js
@@ -1,6 +1,6 @@
 (function () {
     const sec = 31;
-    const events = ['keydown', 'mousemove', 'scroll', 'wheel', 'click'];
+    const events = ['keydown', 'mousemove', 'scroll', 'wheel', 'touchstart', 'click'];
     const screensaver = document.querySelector('#screensaver');
     let timeoutId;
 
@@ -31,3 +31,20 @@
     setTimer();
     setEvents(reset);
 })();
+
+const backBtn = document.createElement('button');
+backBtn.id = "backBtn"
+backBtn.className = "noprint"
+backBtn.textContent = "↵"
+backBtn.type = "button"
+backBtn.style.position = "fixed"
+backBtn.style.bottom = "0.5rem"
+backBtn.style.right = "0.5rem"
+backBtn.style.fontSize = "200%"
+backBtn.style.width = "2.5rem"
+backBtn.style.height = "2.5rem"
+document.body.appendChild(backBtn);
+
+backBtn.addEventListener('click', function () {
+    history.back(-1);
+});
