99b259ecd18ea86a72e7f3f7995c043c3d834f94
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-09-23 11:18:19 +0900

    jp

--- 2019/screensaver.js
+++ 2019/screensaver.js
@@ -31,20 +31,3 @@
     setTimer();
     setEvents(reset);
 })();
-
-const backBtn = document.createElement('button');
-backBtn.id = "backBtn"
-backBtn.className = "noprint"
-backBtn.textContent = "↵"
-backBtn.type = "button"
-backBtn.style.position = "fixed"
-backBtn.style.bottom = "0.5rem"
-backBtn.style.right = "0.5rem"
-backBtn.style.fontSize = "200%"
-backBtn.style.width = "2.5rem"
-backBtn.style.height = "2.5rem"
-document.body.appendChild(backBtn);
-
-backBtn.addEventListener('click', function () {
-    history.back(-1);
-});
