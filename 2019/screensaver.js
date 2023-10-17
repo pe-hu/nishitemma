10363f15d12433ffc31dd3eb5b1e7695b4535c7f
SK-Ⅱ <>
2023-10-17 16:21:19 +0900

    2019

--- 2019/screensaver.js
+++ 2019/screensaver.js
@@ -1,4 +1,33 @@
-(function () {
+document.addEventListener('readystatechange', event => {
+    if (event.target.readyState === 'interactive') {
+        fetchHTML('../../menu.html', '#menu');
+        screensaver();
+    } else if (event.target.readyState === 'complete') {
+        const scrollElement = document.querySelector('main ul');
+        scrollElement.addEventListener('wheel', (e) => {
+            if (Math.abs(e.deltaY) < Math.abs(e.deltaX)) return;
+            const maxScrollLeft = scrollElement.scrollWidth - scrollElement.clientWidth;
+            if (
+                (scrollElement.scrollLeft <= 0 && e.deltaY < 0) ||
+                (scrollElement.scrollLeft >= maxScrollLeft && e.deltaY > 0)
+            )
+                return;
+
+            e.preventDefault();
+            scrollElement.scrollLeft += e.deltaY;
+        });
+    }
+});
+
+async function fetchHTML(url = '', query = '') {
+    fetch(url)
+        .then(response => response.text())
+        .then(innerHTML => {
+            document.querySelector(query).innerHTML = innerHTML
+        });
+}
+
+function screensaver() {
     const sec = 31;
     const events = ['keydown', 'mousemove', 'scroll', 'wheel', 'touchstart', 'click'];
     const screensaver = document.querySelector('#screensaver');
@@ -30,4 +59,4 @@
 
     setTimer();
     setEvents(reset);
-})();
+}
